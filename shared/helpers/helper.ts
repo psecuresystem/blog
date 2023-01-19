3e499cf4901bd9e1cd5a195c892cfeaa62efd828
psecuresystem <92157374+psecuresystem@users.noreply.github.com>
2023-01-19 14:12:49 +0000

    Only blog articles

--- shared/helpers/helper.ts
+++
@@ -1,7 +0,0 @@
-export function concatenateStrings(...args: string[]): string {
-  return args.join(',');
-}
-
-export const getNavigationLink = (slug): string => `/post/${slug}`;
-
-export const getHref = (): string => `/post/[slug]`;
